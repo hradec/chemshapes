b3182735fd4c4d20dca53ce150b1ce6d9c60be30
rhradec <hradec@hradec.com>
2012-02-13 11:18:32 -0800

    remove debug python installs to make the repository simpler to checkout/submit. debug is still in experimental branche. Will move it to a new debug Branch!

--- host/__debug_env_osx/2.7/lib/cmake/PySide-1.0.4/PySideConfig-python2.7.cmake
+++
@@ -1,9 +0,0 @@
-#  PYSIDE_INCLUDE_DIR   - Directories to include to use PySide
-#  PYSIDE_LIBRARY       - Files to link against to use PySide
-#  PYSIDE_PYTHONPATH    - Path to where the PySide Python module files could be found
-#  PYSIDE_TYPESYSTEMS   - Type system files that should be used by other bindings extending PySide
-
-SET(PYSIDE_INCLUDE_DIR "/Volumes/ramdisk/install/usr/include/PySide")
-SET(PYSIDE_LIBRARY "/Volumes/ramdisk/install/usr/lib/libpyside-python2.7.dylib")
-SET(PYSIDE_PYTHONPATH "/Volumes/ramdisk/install/site-packages")
-SET(PYSIDE_TYPESYSTEMS "/Volumes/ramdisk/install/usr/share/PySide/typesystems")
